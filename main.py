c74026d8323d9bec9da9dd00347807cb7d353664
Seokjun Hong <sjhong@ocube.co.kr>
2023-03-13 09:48:46 +0900

    [feat] 예외처리

--- main.py
+++ main.py
@@ -13,28 +13,7 @@ def millis_to_hhmmss(millis):
     print(timedelta(milliseconds=millis))
 
 
-def get_mr_scraping(url):
-    """
-    WebScraping 방식(BeautifulSoup, Selenium)으로 mr(Most Replayed) 그래프 getting
-    *광고 뜨면 동작 안함
-     -> 버림
-    :param url: 대상 유튜브 url
-    :return: mr graph path 값
-    """
-    webdriver_service = Service("./chromedriver")
-    driver = webdriver.Chrome(service=webdriver_service)
-
-    driver.get(url)
-    driver.maximize_window()
-    time.sleep(8)
-
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-
-    graph_path = soup.find("path", {"class": "ytp-heat-map-path"}).get('d')
-    return graph_path
-
-
-def get_mr_scraping_mk2(url: str, count: int = 5):
+def get_mr_scraping(url: str, count: int = 5):
     """
     ytInitialData Scraping 방식
     브라우저를 통한 접속과 requests의 response에 차이가 있음. (아마 유튜브 측에서 로봇임을 감지해 다른 값을 준듯함)
@@ -51,17 +30,18 @@ def get_mr_scraping_mk2(url: str, count: int = 5):
     :param count: 리턴 갯수, default=5
     :return: '동영상 중 replayed 빈도가 높은 순간' millisecond 단위의 list (descending)
     """
-    soup = BeautifulSoup(requests.get(url).text, "html.parser")
-    data = re.search(r"var ytInitialData = ({.*?});", soup.prettify()).group(1)
-    markers_map = json.loads(data)['playerOverlays']['playerOverlayRenderer']['decoratedPlayerBarRenderer'][
-        'decoratedPlayerBarRenderer']['playerBar']['multiMarkersPlayerBarRenderer']['markersMap']
-    heat_seeker = []
-
-    # 챕터가 나눠진 동영상 대응
-    for d in markers_map:
-        if d['key'] == 'HEATSEEKER':
-            heat_seeker = d
     try:
+        soup = BeautifulSoup(requests.get(url).text, "html.parser")
+        data = re.search(r"var ytInitialData = ({.*?});", soup.prettify()).group(1)
+        markers_map = json.loads(data)['playerOverlays']['playerOverlayRenderer']['decoratedPlayerBarRenderer'][
+            'decoratedPlayerBarRenderer']['playerBar']['multiMarkersPlayerBarRenderer']['markersMap']
+        heat_seeker = []
+
+        # 챕터가 나눠진 동영상 대응
+        for d in markers_map:
+            if d['key'] == 'HEATSEEKER':
+                heat_seeker = d
+
         heatmap_array = \
             heat_seeker['value']['heatmap']['heatmapRenderer']['heatMarkers']
         heatmap_sorted_array = sorted(heatmap_array,
@@ -73,8 +53,10 @@ def get_mr_scraping_mk2(url: str, count: int = 5):
         list(map(millis_to_hhmmss, result))
 
         return result
-    except Exception as e:
-        return "ERROR >> " + str(e)
+    except (KeyError, TypeError):  # MostReplayed 정보가 없음
+        return 'YT404'
+    except AttributeError:  # 요청한 URL이 올바르지 않음
+        return 'YT400'
 
 
-print(get_mr_scraping_mk2('https://www.youtube.com/watch?v=B-9I1tDBg60', 10))
+print(get_mr_scraping('https://www.youtube.com/watch?v=8KsEgtUOuvA', 10))
